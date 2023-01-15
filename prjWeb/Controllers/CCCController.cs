4d2cde83784bb1594072c9ccfe0e3d616837a13d
重製 <hpu970390@gmail.com>
2023-01-16 00:22:58 +0800

    CCC

---
+++ prjWeb/Controllers/CCCController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjWeb.Controllers
+{
+    public class CCCController : Controller
+    {
+        // GET: CCC
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
